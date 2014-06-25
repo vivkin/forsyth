3021fe4a488bccc5b8ff4fe31e209791ea7cb241
Ivan Vashchaev <vivkin@gmail.com>
2014-06-25 22:19:29 +0400

    Convert tabs to spaces

--- forsyth.h
+++ forsyth.h
@@ -95,247 +95,244 @@ static ForsythScoreType forsythValenceScore[FORSYTH_VALENCE_SCORE_TABLE_SIZE];
 
 // Precalculate the tables
 static void forsythInit() {
-	for (int i = 0; i < FORSYTH_CACHE_SCORE_TABLE_SIZE; i++) {
-		float score = 0;
-		if (i < 3) {
-			// This vertex was used in the last triangle,
-			// so it has a fixed score, which ever of the three
-			// it's in. Otherwise, you can get very different
-			// answers depending on whether you add
-			// the triangle 1,2,3 or 3,1,2 - which is silly
-			score = FORSYTH_LAST_TRI_SCORE;
-		} else {
-			// Points for being high in the cache.
-			const float scaler = 1.0f / (FORSYTH_CACHE_FUNCTION_LENGTH - 3);
-			score = 1.0f - (i - 3) * scaler;
-			score = powf(score, FORSYTH_CACHE_DECAY_POWER);
-		}
-		forsythCachePositionScore[i] = (ForsythScoreType)(FORSYTH_SCORE_SCALING * score);
-	}
+    for (int i = 0; i < FORSYTH_CACHE_SCORE_TABLE_SIZE; i++) {
+        float score = 0;
+        if (i < 3) {
+            // This vertex was used in the last triangle,
+            // so it has a fixed score, which ever of the three
+            // it's in. Otherwise, you can get very different
+            // answers depending on whether you add
+            // the triangle 1,2,3 or 3,1,2 - which is silly
+            score = FORSYTH_LAST_TRI_SCORE;
+        } else {
+            // Points for being high in the cache.
+            const float scaler = 1.0f / (FORSYTH_CACHE_FUNCTION_LENGTH - 3);
+            score = 1.0f - (i - 3) * scaler;
+            score = powf(score, FORSYTH_CACHE_DECAY_POWER);
+        }
+        forsythCachePositionScore[i] = (ForsythScoreType)(FORSYTH_SCORE_SCALING * score);
+    }
 
-	for (int i = 1; i < FORSYTH_VALENCE_SCORE_TABLE_SIZE; i++) {
-		// Bonus points for having a low number of tris still to
-		// use the vert, so we get rid of lone verts quickly
-		float valenceBoost = powf(i, -FORSYTH_VALENCE_BOOST_POWER);
-		float score = FORSYTH_VALENCE_BOOST_SCALE * valenceBoost;
-		forsythValenceScore[i] = (ForsythScoreType)(FORSYTH_SCORE_SCALING * score);
-	}
+    for (int i = 1; i < FORSYTH_VALENCE_SCORE_TABLE_SIZE; i++) {
+        // Bonus points for having a low number of tris still to
+        // use the vert, so we get rid of lone verts quickly
+        float valenceBoost = powf(i, -FORSYTH_VALENCE_BOOST_POWER);
+        float score = FORSYTH_VALENCE_BOOST_SCALE * valenceBoost;
+        forsythValenceScore[i] = (ForsythScoreType)(FORSYTH_SCORE_SCALING * score);
+    }
 }
 
 // Calculate the score for a vertex
 static ForsythScoreType forsythFindVertexScore(int numActiveTris, int cachePosition) {
-	if (numActiveTris == 0) {
-		// No triangles need this vertex!
-		return 0;
-	}
+    if (numActiveTris == 0) {
+        // No triangles need this vertex!
+        return 0;
+    }
 
-	ForsythScoreType score = 0;
-	if (cachePosition < 0) {
-		// Vertex is not in LRU cache - no score
-	} else {
-		score = forsythCachePositionScore[cachePosition];
-	}
+    ForsythScoreType score = 0;
+    if (cachePosition < 0) {
+        // Vertex is not in LRU cache - no score
+    } else {
+        score = forsythCachePositionScore[cachePosition];
+    }
 
-	if (numActiveTris < FORSYTH_VALENCE_SCORE_TABLE_SIZE)
-		score += forsythValenceScore[numActiveTris];
-	return score;
+    if (numActiveTris < FORSYTH_VALENCE_SCORE_TABLE_SIZE)
+        score += forsythValenceScore[numActiveTris];
+    return score;
 }
 
 // The main reordering function
 int forsythReorderIndices(ForsythVertexIndexType *outIndices, const ForsythVertexIndexType *indices, int nTriangles, int nVertices) {
-	// The tables need not be inited every time this function
-	// is used. Either call initForsyth from the calling process,
-	// or just replace the score tables with precalculated values.
-	static int init = 1;
-	if (init) {
-		forsythInit();
-		init = 0;
-	}
+    static int init = 1;
+    if (init) {
+        forsythInit();
+        init = 0;
+    }
 
-	ForsythAdjacencyType *numActiveTris = (ForsythAdjacencyType *)malloc(sizeof(ForsythAdjacencyType) * nVertices);
-	memset(numActiveTris, 0, sizeof(ForsythAdjacencyType) * nVertices);
+    ForsythAdjacencyType *numActiveTris = (ForsythAdjacencyType *)malloc(sizeof(ForsythAdjacencyType) * nVertices);
+    memset(numActiveTris, 0, sizeof(ForsythAdjacencyType) * nVertices);
 
-	// First scan over the vertex data, count the total number of
-	// occurrances of each vertex
-	for (int i = 0; i < 3 * nTriangles; i++) {
-		if (numActiveTris[indices[i]] == FORSYTH_MAX_ADJACENCY) {
-			// Unsupported mesh,
-			// vertex shared by too many triangles
-			free(numActiveTris);
-			return 0;
-		}
-		numActiveTris[indices[i]]++;
-	}
+    // First scan over the vertex data, count the total number of
+    // occurrances of each vertex
+    for (int i = 0; i < 3 * nTriangles; i++) {
+        if (numActiveTris[indices[i]] == FORSYTH_MAX_ADJACENCY) {
+            // Unsupported mesh,
+            // vertex shared by too many triangles
+            free(numActiveTris);
+            return 0;
+        }
+        numActiveTris[indices[i]]++;
+    }
 
-	// Allocate the rest of the arrays
-	ForsythArrayIndexType *offsets = (ForsythArrayIndexType *)malloc(sizeof(ForsythArrayIndexType) * nVertices);
-	ForsythScoreType *lastScore = (ForsythScoreType *)malloc(sizeof(ForsythScoreType) * nVertices);
-	ForsythCachePosType *cacheTag = (ForsythCachePosType *)malloc(sizeof(ForsythCachePosType) * nVertices);
+    // Allocate the rest of the arrays
+    ForsythArrayIndexType *offsets = (ForsythArrayIndexType *)malloc(sizeof(ForsythArrayIndexType) * nVertices);
+    ForsythScoreType *lastScore = (ForsythScoreType *)malloc(sizeof(ForsythScoreType) * nVertices);
+    ForsythCachePosType *cacheTag = (ForsythCachePosType *)malloc(sizeof(ForsythCachePosType) * nVertices);
 
-	uint8_t *triangleAdded = (uint8_t *)malloc((nTriangles + 7) / 8);
-	ForsythScoreType *triangleScore = (ForsythScoreType *)malloc(sizeof(ForsythScoreType) * nTriangles);
-	ForsythTriangleIndexType *triangleIndices = (ForsythTriangleIndexType *)malloc(sizeof(ForsythTriangleIndexType) * 3 * nTriangles);
-	memset(triangleAdded, 0, sizeof(uint8_t) * ((nTriangles + 7) / 8));
-	memset(triangleScore, 0, sizeof(ForsythScoreType) * nTriangles);
-	memset(triangleIndices, 0, sizeof(ForsythTriangleIndexType) * 3 * nTriangles);
+    uint8_t *triangleAdded = (uint8_t *)malloc((nTriangles + 7) / 8);
+    ForsythScoreType *triangleScore = (ForsythScoreType *)malloc(sizeof(ForsythScoreType) * nTriangles);
+    ForsythTriangleIndexType *triangleIndices = (ForsythTriangleIndexType *)malloc(sizeof(ForsythTriangleIndexType) * 3 * nTriangles);
+    memset(triangleAdded, 0, sizeof(uint8_t) * ((nTriangles + 7) / 8));
+    memset(triangleScore, 0, sizeof(ForsythScoreType) * nTriangles);
+    memset(triangleIndices, 0, sizeof(ForsythTriangleIndexType) * 3 * nTriangles);
 
-	// Count the triangle array offset for each vertex,
-	// initialize the rest of the data.
-	int sum = 0;
-	for (int i = 0; i < nVertices; i++) {
-		offsets[i] = sum;
-		sum += numActiveTris[i];
-		numActiveTris[i] = 0;
-		cacheTag[i] = -1;
-	}
+    // Count the triangle array offset for each vertex,
+    // initialize the rest of the data.
+    int sum = 0;
+    for (int i = 0; i < nVertices; i++) {
+        offsets[i] = sum;
+        sum += numActiveTris[i];
+        numActiveTris[i] = 0;
+        cacheTag[i] = -1;
+    }
 
-	// Fill the vertex data structures with indices to the triangles
-	// using each vertex
-	for (int i = 0; i < nTriangles; i++) {
-		for (int j = 0; j < 3; j++) {
-			int v = indices[3 * i + j];
-			triangleIndices[offsets[v] + numActiveTris[v]] = i;
-			numActiveTris[v]++;
-		}
-	}
+    // Fill the vertex data structures with indices to the triangles
+    // using each vertex
+    for (int i = 0; i < nTriangles; i++) {
+        for (int j = 0; j < 3; j++) {
+            int v = indices[3 * i + j];
+            triangleIndices[offsets[v] + numActiveTris[v]] = i;
+            numActiveTris[v]++;
+        }
+    }
 
-	// Initialize the score for all vertices
-	for (int i = 0; i < nVertices; i++) {
-		lastScore[i] = forsythFindVertexScore(numActiveTris[i], cacheTag[i]);
-		for (int j = 0; j < numActiveTris[i]; j++)
-			triangleScore[triangleIndices[offsets[i] + j]] += lastScore[i];
-	}
+    // Initialize the score for all vertices
+    for (int i = 0; i < nVertices; i++) {
+        lastScore[i] = forsythFindVertexScore(numActiveTris[i], cacheTag[i]);
+        for (int j = 0; j < numActiveTris[i]; j++)
+            triangleScore[triangleIndices[offsets[i] + j]] += lastScore[i];
+    }
 
-	// Find the best triangle
-	int bestTriangle = -1;
-	int bestScore = -1;
+    // Find the best triangle
+    int bestTriangle = -1;
+    int bestScore = -1;
 
-	for (int i = 0; i < nTriangles; i++) {
-		if (triangleScore[i] > bestScore) {
-			bestScore = triangleScore[i];
-			bestTriangle = i;
-		}
-	}
+    for (int i = 0; i < nTriangles; i++) {
+        if (triangleScore[i] > bestScore) {
+            bestScore = triangleScore[i];
+            bestTriangle = i;
+        }
+    }
 
-	// Allocate the output array
-	ForsythTriangleIndexType *outTriangles = (ForsythTriangleIndexType *)malloc(sizeof(ForsythTriangleIndexType) * nTriangles);
-	int outPos = 0;
+    // Allocate the output array
+    ForsythTriangleIndexType *outTriangles = (ForsythTriangleIndexType *)malloc(sizeof(ForsythTriangleIndexType) * nTriangles);
+    int outPos = 0;
 
-	// Initialize the cache
-	int cache[FORSYTH_VERTEX_CACHE_SIZE + 3];
-	for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE + 3; i++)
-		cache[i] = -1;
+    // Initialize the cache
+    int cache[FORSYTH_VERTEX_CACHE_SIZE + 3];
+    for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE + 3; i++)
+        cache[i] = -1;
 
-	int scanPos = 0;
+    int scanPos = 0;
 
-	// Output the currently best triangle, as long as there
-	// are triangles left to output
-	while (bestTriangle >= 0) {
-		// Mark the triangle as added
-		FORSYTH_SETADDED(bestTriangle);
-		// Output this triangle
-		outTriangles[outPos++] = bestTriangle;
-		for (int i = 0; i < 3; i++) {
-			// Update this vertex
-			int v = indices[3 * bestTriangle + i];
+    // Output the currently best triangle, as long as there
+    // are triangles left to output
+    while (bestTriangle >= 0) {
+        // Mark the triangle as added
+        FORSYTH_SETADDED(bestTriangle);
+        // Output this triangle
+        outTriangles[outPos++] = bestTriangle;
+        for (int i = 0; i < 3; i++) {
+            // Update this vertex
+            int v = indices[3 * bestTriangle + i];
 
-			// Check the current cache position, if it
-			// is in the cache
-			int endpos = cacheTag[v];
-			if (endpos < 0)
-				endpos = FORSYTH_VERTEX_CACHE_SIZE + i;
-			// Move all cache entries from the previous position
-			// in the cache to the new target position (i) one
-			// step backwards
-			for (int j = endpos; j > i; j--) {
-				cache[j] = cache[j - 1];
-				// If this cache slot contains a real
-				// vertex, update its cache tag
-				if (cache[j] >= 0)
-					cacheTag[cache[j]]++;
-			}
-			// Insert the current vertex into its new target
-			// slot
-			cache[i] = v;
-			cacheTag[v] = i;
+            // Check the current cache position, if it
+            // is in the cache
+            int endpos = cacheTag[v];
+            if (endpos < 0)
+                endpos = FORSYTH_VERTEX_CACHE_SIZE + i;
+            // Move all cache entries from the previous position
+            // in the cache to the new target position (i) one
+            // step backwards
+            for (int j = endpos; j > i; j--) {
+                cache[j] = cache[j - 1];
+                // If this cache slot contains a real
+                // vertex, update its cache tag
+                if (cache[j] >= 0)
+                    cacheTag[cache[j]]++;
+            }
+            // Insert the current vertex into its new target
+            // slot
+            cache[i] = v;
+            cacheTag[v] = i;
 
-			// Find the current triangle in the list of active
-			// triangles and remove it (moving the last
-			// triangle in the list to the slot of this triangle).
-			for (int j = 0; j < numActiveTris[v]; j++) {
-				if (triangleIndices[offsets[v] + j] == bestTriangle) {
-					triangleIndices[offsets[v] + j] = triangleIndices[offsets[v] + numActiveTris[v] - 1];
-					break;
-				}
-			}
-			// Shorten the list
-			numActiveTris[v]--;
-		}
-		// Update the scores of all triangles in the cache
-		for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE + 3; i++) {
-			int v = cache[i];
-			if (v < 0)
-				break;
-			// This vertex has been pushed outside of the
-			// actual cache
-			if (i >= FORSYTH_VERTEX_CACHE_SIZE) {
-				cacheTag[v] = -1;
-				cache[i] = -1;
-			}
-			ForsythScoreType newScore = forsythFindVertexScore(numActiveTris[v], cacheTag[v]);
-			ForsythScoreType diff = newScore - lastScore[v];
-			for (int j = 0; j < numActiveTris[v]; j++)
-				triangleScore[triangleIndices[offsets[v] + j]] += diff;
-			lastScore[v] = newScore;
-		}
-		// Find the best triangle referenced by vertices in the cache
-		bestTriangle = -1;
-		bestScore = -1;
-		for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE; i++) {
-			if (cache[i] < 0)
-				break;
-			int v = cache[i];
-			for (int j = 0; j < numActiveTris[v]; j++) {
-				int t = triangleIndices[offsets[v] + j];
-				if (triangleScore[t] > bestScore) {
-					bestTriangle = t;
-					bestScore = triangleScore[t];
-				}
-			}
-		}
-		// If no active triangle was found at all, continue
-		// scanning the whole list of triangles
-		if (bestTriangle < 0) {
-			for (; scanPos < nTriangles; scanPos++) {
-				if (!FORSYTH_ISADDED(scanPos)) {
-					bestTriangle = scanPos;
-					break;
-				}
-			}
-		}
-	}
+            // Find the current triangle in the list of active
+            // triangles and remove it (moving the last
+            // triangle in the list to the slot of this triangle).
+            for (int j = 0; j < numActiveTris[v]; j++) {
+                if (triangleIndices[offsets[v] + j] == bestTriangle) {
+                    triangleIndices[offsets[v] + j] = triangleIndices[offsets[v] + numActiveTris[v] - 1];
+                    break;
+                }
+            }
+            // Shorten the list
+            numActiveTris[v]--;
+        }
+        // Update the scores of all triangles in the cache
+        for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE + 3; i++) {
+            int v = cache[i];
+            if (v < 0)
+                break;
+            // This vertex has been pushed outside of the
+            // actual cache
+            if (i >= FORSYTH_VERTEX_CACHE_SIZE) {
+                cacheTag[v] = -1;
+                cache[i] = -1;
+            }
+            ForsythScoreType newScore = forsythFindVertexScore(numActiveTris[v], cacheTag[v]);
+            ForsythScoreType diff = newScore - lastScore[v];
+            for (int j = 0; j < numActiveTris[v]; j++)
+                triangleScore[triangleIndices[offsets[v] + j]] += diff;
+            lastScore[v] = newScore;
+        }
+        // Find the best triangle referenced by vertices in the cache
+        bestTriangle = -1;
+        bestScore = -1;
+        for (int i = 0; i < FORSYTH_VERTEX_CACHE_SIZE; i++) {
+            if (cache[i] < 0)
+                break;
+            int v = cache[i];
+            for (int j = 0; j < numActiveTris[v]; j++) {
+                int t = triangleIndices[offsets[v] + j];
+                if (triangleScore[t] > bestScore) {
+                    bestTriangle = t;
+                    bestScore = triangleScore[t];
+                }
+            }
+        }
+        // If no active triangle was found at all, continue
+        // scanning the whole list of triangles
+        if (bestTriangle < 0) {
+            for (; scanPos < nTriangles; scanPos++) {
+                if (!FORSYTH_ISADDED(scanPos)) {
+                    bestTriangle = scanPos;
+                    break;
+                }
+            }
+        }
+    }
 
-	// Convert the triangle index array into a full triangle list
-	outPos = 0;
-	for (int i = 0; i < nTriangles; i++) {
-		int t = outTriangles[i];
-		for (int j = 0; j < 3; j++) {
-			int v = indices[3 * t + j];
-			outIndices[outPos++] = v;
-		}
-	}
+    // Convert the triangle index array into a full triangle list
+    outPos = 0;
+    for (int i = 0; i < nTriangles; i++) {
+        int t = outTriangles[i];
+        for (int j = 0; j < 3; j++) {
+            int v = indices[3 * t + j];
+            outIndices[outPos++] = v;
+        }
+    }
 
-	// Clean up
-	free(triangleIndices);
-	free(offsets);
-	free(lastScore);
-	free(numActiveTris);
-	free(cacheTag);
-	free(triangleAdded);
-	free(triangleScore);
-	free(outTriangles);
+    // Clean up
+    free(triangleIndices);
+    free(offsets);
+    free(lastScore);
+    free(numActiveTris);
+    free(cacheTag);
+    free(triangleAdded);
+    free(triangleScore);
+    free(outTriangles);
 
-	return 1;
+    return 1;
 }
 
 #ifdef __cplusplus
